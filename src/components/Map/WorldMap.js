558db75bc8c2e1f58135b224a18af48cff123f7d
Ioana Verebi <ioanaverebi@gmail.com>
2017-04-29 16:11:09 +0300

    Added GeoJSONLayer

--- src/components/Map/WorldMap.js
+++ src/components/Map/WorldMap.js
@@ -1,11 +1,18 @@
+/* eslint-disable */
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { GeoJSONLayer, ScaleControl, ZoomControl, Feature } from "react-mapbox-gl";
+import birds from "./birds.json";
 
 class WorldMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      center: [ 45.7488716, 21.20867929999997 ]
     }
+  }
+
+  componentDidMount(){
+
   }
 
   render() {
@@ -13,16 +20,25 @@ class WorldMap extends Component {
       <ReactMapboxGl
         style="mapbox://styles/alexmf3/cj1w59evy00262so0kefbbrhg"
         accessToken="pk.eyJ1IjoiYWxleG1mMyIsImEiOiJjajF2OHF6NHAwMDEwMnFuenphY3o5cG13In0.awCN0YN3--_wVfp7r-xjgA"
+        center={this.state.center}
         containerStyle={{
           height: "100vh",
           width: "100vw"
         }}>
-          <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}>
-            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-          </Layer>
+        <ScaleControl/>
+          <GeoJSONLayer
+          // use here the URL with GeoJSON
+          // example live data: https://www.mapbox.com/mapbox-gl-js/example/live-geojson/
+            data={birds}
+            circleLayout={{ visibility: "visible" }}
+            symbolLayout={{
+              "text-field": "{place}",
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+            }}
+          />
+
       </ReactMapboxGl>
     )
   }
