4f91b32e260c3ba1708c566d86835c2274a543d9
AlexMarin <alex@birdseye.space>
2017-05-08 23:38:06 +0300

    fixed markers

--- src/components/Map/WorldMapConfig.js
+++ src/components/Map/WorldMapConfig.js
@@ -1,6 +1,6 @@
 export const MAPBOX_CONFIG = {
     'explore': {
-      mapStyle: "mapbox://styles/alexmf3/cj2aeeeh500572sn0ztpdfz4o",
+      mapStyle: "mapbox://styles/alexmf3/cj2adeed0005b2srt4qaar8xy",
       mapToken: "pk.eyJ1IjoiYWxleG1mMyIsImEiOiJjajF2OHF6NHAwMDEwMnFuenphY3o5cG13In0.awCN0YN3--_wVfp7r-xjgA"
     },
     'visualize': {
